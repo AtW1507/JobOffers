81ebd72bd73042cd1616751577f8368cbc9527d7
Adrian Wilczek <adrian.wilczek@onet.com.pl>
2026-07-14 17:08:51 +0200

    deleted no using imports

--- src/main/java/com/junioroffer/domain/offer/dto/JobOfferResponse.java
+++ src/main/java/com/junioroffer/domain/offer/dto/JobOfferResponse.java
@@ -1,11 +1,7 @@
 package com.junioroffer.domain.offer.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
-
-
-import java.util.List;
 
 
 @Builder
