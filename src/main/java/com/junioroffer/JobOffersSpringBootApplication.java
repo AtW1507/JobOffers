96692b5bbdc59f2a28bad52c70faf54e730229c4
Adrian Wilczek <adrian.wilczek@onet.com.pl>
2026-08-30 16:51:51 +0200

    Updated external job offers API URL and port

--- src/main/java/com/junioroffer/JobOffersSpringBootApplication.java
+++ src/main/java/com/junioroffer/JobOffersSpringBootApplication.java
@@ -5,14 +5,11 @@ import com.junioroffer.infrastructure.security.jwt.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({OfferHttpClientRestTemplateConfigurationProperties.class, JwtConfigurationProperties.class})
 @EnableMongoRepositories
-//@EnableScheduling
 public class JobOffersSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobOffersSpringBootApplication.class, args);
